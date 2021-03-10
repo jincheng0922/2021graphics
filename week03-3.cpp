060ed2b9d1a4f2c0d917f3186732844c9fe75389
jincheng0922 <80370510+jincheng0922@users.noreply.github.com>
2021-03-10 11:58:41 +0800

    Add files via upload

---
+++ week03-3.cpp
@@ -0,0 +1,25 @@
+#include <GL/glut.h>
+#include <math.h>
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+
+    glColor3f(1,1,0);
+    glBegin(GL_POLYGON);
+    for(int i=0;i<30;i++){
+        float a=3.1415926*2/30*i;
+        glVertex2f(0.5+0.1*cos(a),0.2*sin(a));
+    }
+    glEnd();
+    glutSwapBuffers();
+}
+int main(int argc,char**argv)
+{
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
+    glutCreateWindow("Week-circle");
+
+    glutDisplayFunc(display);
+
+    glutMainLoop();
+}
