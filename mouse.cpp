5129ffff487d719560a2ed8a484be02c4326f17f
jincheng0922 <80370510+jincheng0922@users.noreply.github.com>
2021-03-17 10:56:10 +0800

    Add files via upload

---
+++ mouse.cpp
@@ -0,0 +1,23 @@
+#include <GL/glut.h>
+#include <stdio.h>
+
+static void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glutSolidTeapot(0.3);
+    glutSwapBuffers();
+}
+void mouse(int button,int state,int x,int y)
+{
+    printf("button:%d state:%d x:%d y:%d\n",button,state,x,y);
+}
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("08160563 Good!");
+    glutDisplayFunc(display);
+    glutMouseFunc(mouse);
+    glutMainLoop();
+}
